5e845de69f3d4ec6296de54e6e168eafa5edb864
Jeff Breig <jjbreig@gmail.com>
2019-12-19 21:44:06 -0500

    starting app template

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import RecipePage from "./components/ChefPortfolio/RecipePage"
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Route path="/recipepage/" component={RecipePage} />
+      </Router>
     </div>
   );
 }
