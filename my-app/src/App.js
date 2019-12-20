e09c7051d0c63ebbabcda405840403860f61cdc5
Jeff Breig <jjbreig@gmail.com>
2019-12-19 22:51:17 -0500

    started nav-bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,15 +6,25 @@ import RecipePage from "./components/ChefPortfolio/RecipePage"
 import './App.css';
 import ChefPortfolioPage from './components/ChefPortfolio/ChefPortfolioPage';
 
+import styled from "styled-components";
+
+const NavBar=styled.nav`
+  display: flex;
+  justify-items: space-around;
+
+`
+
 
 function App() {
   return (
     <div>
-      <Router>
+      <NavBar>
+        Testing
+      </NavBar>
         <Route path="/recipepage/" component={RecipePage} />
         {/* will change ChefPortolioPage to Private Route later */}
         <Route path= '/chefportfolio/' component = {ChefPortfolioPage} />
-      </Router>
+      
     </div>
   );
 }
