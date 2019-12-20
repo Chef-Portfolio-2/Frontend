c4c31895f96b2038224fbc03499e161cbeb4b920
Jeff Breig <jjbreig@gmail.com>
2019-12-19 22:37:00 -0500

    jeff-breig

--- my-app/src/components/ChefPortfolio/RecipePage.js
+++ my-app/src/components/ChefPortfolio/RecipePage.js
@@ -6,25 +6,90 @@ import styled from "styled-components";
 const Container = styled.div`
     display: flex; 
     flex-direction: column;
-    width: 70%;
+    width: 50%;
     margin: 5vh auto;
-    border: 1px solid red;
     align-items: center;
 `;
 
 const RecipeHeader = styled.div`
     display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
     
 
 `
 
+const RecipeInfo = styled.div`
+    
+`
+
+
+const RecipeTitle = styled.h2`
+    font-size: 2rem;
+    margin: 10px 0 0px 0;
+`
+
+const RecipeAuthor = styled.h3`
+    font-size: 1rem;
+    color: gray;
+    margin: 5px 0px 5px 0px;
+`
+
+const RecipeContent = styled.p`
+    font-size: 1rem;
+`
+
+const RecipeTags = styled.div`
+  width: 25%;
+`;
+
+const RecipeTag = styled.div`
+  background-color: #32b672;
+  margin-bottom: 5px;
+  border-radius: 15px;
+  text-align: center;
+  width: 100%;
+`;
+
 
 function RecipePage() {
     return (
-        <Container>
-            <img className="recipe-page-image" src="https://i.cbc.ca/1.5191482.1564695162!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/chocolate-tahini-fudge-sundaes.jpg"/>
-            <p>recipeeeees</p>
-        </Container>
+      <Container>
+        <img
+          className="recipe-page-image"
+          src="https://i.cbc.ca/1.5191482.1564695162!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/chocolate-tahini-fudge-sundaes.jpg"
+        />
+        <RecipeHeader>
+          <RecipeInfo>
+            <RecipeTitle>Fancy Sundae Thing</RecipeTitle>
+            <RecipeAuthor>Jeff Breig</RecipeAuthor>
+          </RecipeInfo>
+          <RecipeTags>
+            <RecipeTag>Ice Cream</RecipeTag>
+            <RecipeTag>Dairy</RecipeTag>
+            <RecipeTag>Nuts</RecipeTag>
+          </RecipeTags>
+        </RecipeHeader>
+        <RecipeContent>
+          We'll make some happy little bushes here. Everyone is going to see
+          things differently - and that's the way it should be. Put it in, leave
+          it alone. Just take out whatever you don't want. It'll change your
+          entire perspective. It's a very cold picture, I may have to go get my
+          coat. It’s about to freeze me to death. That easy.
+          <br />
+          <br />
+          Let's get wild today. If you've been in Alaska less than a year you're
+          a Cheechako. You can do anything here. So don't worry about it. It's
+          amazing what you can do with a little love in your heart. These little
+          son of a guns hide in your brush and you just have to push them out.
+          It is a lot of fun.
+          <br />
+          <br />I get carried away with this brush cleaning. Use your
+          imagination, let it go. Don't forget to tell these special people in
+          your life just how special they are to you.
+        </RecipeContent>
+      </Container>
     );
 }
 
