21cfa05e8bf1283d8f17584fc4f402b11eb662fd
Miley Wright <dishaliekwright@yahoo.com>
2019-12-23 03:06:45 -0500

    added in generic image

--- my-app/src/components/ChefPortfolio/ChefPortfolioPage.js
+++ my-app/src/components/ChefPortfolio/ChefPortfolioPage.js
@@ -24,7 +24,7 @@ function ChefPortfolioPage() {
             <div className='headContainer'>
                 <h2> Account </h2>
                 <div>
-                    <img src ='/' /> 
+                    <img src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAiRjeHqC26SHWGgBOlwulRONJSgZXg1XNsybBoa-iPwgH1wHutQ&s' /> 
                     <div className= 'chefInfo'>
                         <p>Name: </p>
                         <p>Email: </p>
