d8362d93d7f9341bbf3bb5d0c0cd1e9f492403d1
Miley Wright <dishaliekwright@yahoo.com>
2020-01-02 08:23:02 -0500

    created state and handles

--- my-app/src/components/ChefPortfolio/Register.js
+++ my-app/src/components/ChefPortfolio/Register.js
@@ -2,8 +2,42 @@ import React, { useState } from 'react';
 import { axiosWithAuth } from '../axiosAuthenticate/axiosWithAuth';
 
 const RegisterChef = () => {
+    const defaultUser = {
+        username: '',
+        password: '',
+        email: '',
+        location: ''
+    };
+
+    const [chef, setChef] = useState(defaultUser);
+
+    const handleChange = (event) => {
+        setChef({ ...chef, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axiosWithAuth()
+        .post('/auth/register/', chef)
+        .then((res) => {
+            console.log(res);
+            localStorage.setItem('token', res.data.token);
+            localStorage.setItem('id',res.data.id);
+            localStorage.setItem('username', res.data.username);
+
+            setChef(defaultUser);
+        })
+        .catch((err) => console.log(err));
+    }
+
     return (
         <>
+            <RegDiv>
+                <h1>Create an Account</h1>
+                <form autoComplete='new-password' onSubmit={handleSubmit}>
+                    
+                </form>
+            </RegDiv>
         </>
     )
 }
