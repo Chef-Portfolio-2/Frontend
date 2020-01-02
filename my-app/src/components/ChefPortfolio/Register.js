630cbd3f0a475057f5b982cd3dc8ecbc79c6ea9c
Miley Wright <dishaliekwright@yahoo.com>
2020-01-02 07:45:30 -0500

    setting up the Register page

---
+++ my-app/src/components/ChefPortfolio/Register.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../axiosAuthenticate/axiosWithAuth';
+
+const RegisterChef = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default RegisterChef
