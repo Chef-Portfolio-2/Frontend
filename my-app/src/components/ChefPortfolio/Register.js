a362e1c0b0fb31a1c527b7df122c4f62be20e186
Miley Wright <dishaliekwright@yahoo.com>
2020-01-02 08:39:53 -0500

    completed register form. Need to style

--- my-app/src/components/ChefPortfolio/Register.js
+++ my-app/src/components/ChefPortfolio/Register.js
@@ -3,6 +3,8 @@ import { axiosWithAuth } from '../axiosAuthenticate/axiosWithAuth';
 
 const RegisterChef = () => {
     const defaultUser = {
+        firstname: '',
+        lastname: '',
         username: '',
         password: '',
         email: '',
@@ -32,12 +34,62 @@ const RegisterChef = () => {
 
     return (
         <>
-            <RegDiv>
+            <div>
                 <h1>Create an Account</h1>
                 <form autoComplete='new-password' onSubmit={handleSubmit}>
-                    
+                <input
+                        type='text'
+                        name='firstname'
+                        placeholder='First Name'
+                        value={chef.firstname}
+                        onChange={handleChange}
+                        className='inputField'
+                    />
+                    <input
+                        type='text'
+                        name='Last Name'
+                        placeholder='Last Name'
+                        value={chef.lastname}
+                        onChange={handleChange}
+                        className='inputField'
+                    />
+                    <input
+                        type='text'
+                        name='username'
+                        placeholder='Username'
+                        value={chef.username}
+                        onChange={handleChange}
+                        className='inputField'
+                    />
+                    <input
+                        type='email'
+                        name='email'
+                        placeholder='Email'
+                        value={chef.email}
+                        onChange={handleChange}
+                        className='inputField'
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        placeholder='Password'
+                        value={chef.password}
+                        onChange={handleChange}
+                        className='inputField'
+                    />
+                    <input
+                        type='text'
+                        name='location'
+                        placeholder='City'
+                        value={chef.username}
+                        onChange={handleChange}
+                        className='inputField'
+                    />
+
+                    <input type='submit'></input>
+                    <input type='reset'></input>
                 </form>
-            </RegDiv>
+            </div>
         </>
     )
 }
