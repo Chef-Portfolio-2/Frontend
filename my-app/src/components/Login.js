31f002c9292391853f90667f1bc9ba3f2474d482
alexis-anderson <aanderson9313@gmail.com>
2020-01-07 20:24:02 -0500

    reworked login page

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,126 +1,80 @@
-import React, { useState, useEffect } from 'react';
+  
+import React from 'react';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
-import styled from "styled-components";
-import "./Login.css";
-import Axios from 'axios';
+import styles from "styled-components";
+import axios from "axios";
+import '../App.css';
 
+function LoginForm({errors, touched, Values}) {
 
-const Login1 = styled.div`
-height: auto;
-width: auto;
-margin-left: 10%;
-margin-right: 10%;
-padding-top: 20%;
-padding-bottom: 20%;
-`
+    return (
+      <div className="container">
+        <Form
+        //   onSubmit={this.handleSubmit}
+          className="login"
+        >
+          
+          <h1 className="Welcome">
+            Welcome back! Please sign in.
+          </h1>
+          {touched.email && errors.email && <p>{errors.email}</p>} 
+          <Field
+            type="email"
+            name="email"
+            className="emailInput"
+            placeholder="Email"
+            // onChange={this.handleChange}
+          /><br/>
+        {touched.password && errors.password && <p>{errors.password}</p>} 
+          <Field
+            type="password"
+            name="password"
+            className="pwInput"
+            placeholder="Password"
+            // onChange={this.handleChange}
+          /><br/>
+          <button 
+            className="Submit"
+            type="submit"
+          >
+            Login!
+          </button>
+        </Form>
 
-const Login = styled.div`
-display: flex;
-align-content: center;
-justify-content: center;
-`
-const Label = styled.label`
-font-weight: 600;
-font-size: 20px;
-`
-
-const Form1 = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-between;
-padding-bottom: 5%;
-width: 70%;
-`
-const Button = styled.button`
-display: flex;
-margin-left: 50%;
-border: 1px solid white;
-border-radius: 50%;
-height: 60px; 
-width: 60px;
-background-color: #32B672;
-color: white;
-box-shadow: 2px 2px black; 
-`
-const A1 = styled.a`
-display: flex;
-align-content: center;
-justify-content: center;
-margin-left: 40%;
-margin-top: 2%;
-height: auto; 
-width: 30%;
-color: black;
-`
-
-
-
-
-function LoginForm({errors, touched }) {
-
-
-    return(
-        <Login1 className = "loginForm">
-            {console.log()}
-            <Form>
-                <Login>
-                    <Form1>
-                    {touched.email && errors.email && <p>{errors.email}</p>}
-                    {/* shows error message */}
-                    <Label>Email:</Label>
-                    <Field className = "field"
-                    type = "email"
-                    name = "email"
-                    placeholder = "Email"
-                    />
-                    </Form1>
-                </Login>
-               
-                <Login>
-                    <Form1>
-                    {touched.password && errors.password && <p>{errors.email}</p>}
-                    <Label>Password:</Label>
-                    <Field className = "field"
-                    type = "password"
-                    name = "password"
-                    placeholder = "Password"
-                    />
-                    </Form1>
-                </Login>
-                <Button type="submit">Login Here!</Button>
-                <A1 href = "http://localhost:3000/Register/">New User? Register Here!</A1>
-            </Form>
-        </Login1>
+      </div>
     );
 }
 
-const FormikLoginForm = withFormik({
-    mapPropsToValues({ email, password }) {
-        return {
-            email: email || "",
-            password: password || ""
-        };
-    },
+    const FormikLoginForm = withFormik({
 
-    // validation Schema:
-
-    validationSchema: Yup.object().shape({
-        email: Yup.string().email("Email is not Valid").required("Required Field"),
-        password: Yup.string().min(6, "Password must be 6 characters or longer").required("Required Field")
-    }),
-
-    // handle submit
-    handleSubmit(values) {
-        Axios
-            .post("https://reqres.in/api/users/", values)
-            .then(response => {
-                console.log(response);
-                
-            })
-            .catch(error => console.log(error.response));
-    }
-})(LoginForm);
+        mapPropsToValues({ email, password }) {
+            return {
+               email: email || "",
+               password: password || ""  
+            };
+        },
+        // validation schema
+        validationSchema: Yup.object().shape({
+            email: Yup.string()
+              .email("please enter a valid Email")
+              .required("Email is required"),
+            password: Yup.string()
+              .min(6, "password must be 6 characters or longer")
+              .required("Password is required")
+          }),
+        // handle submit
+        handleSubmit(values) {
+              axios
+                .post("https://reqres.in/api/users/", values)
+                .then(res => {
+                  console.log(values); // Data was created successfully and logs to console
+                })
+                .catch(err => {
+                  console.log(err); // There was an error creating the data and logs to console
+                });
+            }
+        // axios post here
+    }) (LoginForm);
 
 export default FormikLoginForm;
