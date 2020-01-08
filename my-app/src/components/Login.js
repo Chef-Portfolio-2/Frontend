7be381cd1bc921c65886a110373db3d110e29b3e
alexis-anderson <aanderson9313@gmail.com>
2020-01-07 21:03:11 -0500

    updating api

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -2,28 +2,56 @@
 import React from 'react';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
-import styles from "styled-components";
+import styled from "styled-components";
 import axios from "axios";
-import '../App.css';
+import './Login.css';
+
+// styles
+
+const Container = styled.div `
+display: flex;
+border: 1px solid black;
+border-radius: 5px;
+margin-left: 35%;
+margin-right: 35%;
+height: auto;
+width: 100%;
+justify-content: center;
+align-content: center;
+background: #32B672;
+
+`
+
+const Welcome = styled.h1 `
+color: white;
+`
+
+
+
+
+
+
+
+
 
 function LoginForm({errors, touched, Values}) {
 
     return (
-      <div className="container">
+      <Container className="container">
         <Form
         //   onSubmit={this.handleSubmit}
           className="login"
         >
           
-          <h1 className="Welcome">
+          <Welcome className="Welcome">
             Welcome back! Please sign in.
-          </h1>
-          {touched.email && errors.email && <p>{errors.email}</p>} 
+          </Welcome>
+          {touched.username && errors.username && <p>{errors.username}</p>} 
           <Field
-            type="email"
-            name="email"
-            className="emailInput"
-            placeholder="Email"
+            type="text"
+            name="username"
+            className="username"
+            placeholder="Username"
             // onChange={this.handleChange}
           /><br/>
         {touched.password && errors.password && <p>{errors.password}</p>} 
@@ -42,23 +70,23 @@ function LoginForm({errors, touched, Values}) {
           </button>
         </Form>
 
-      </div>
+      </Container>
     );
 }
 
     const FormikLoginForm = withFormik({
 
-        mapPropsToValues({ email, password }) {
+        mapPropsToValues({ username, password }) {
             return {
-               email: email || "",
+               username: username || "",
                password: password || ""  
             };
         },
         // validation schema
         validationSchema: Yup.object().shape({
             email: Yup.string()
-              .email("please enter a valid Email")
-              .required("Email is required"),
+              .username("please enter a valid username")
+              .required("username is required"),
             password: Yup.string()
               .min(6, "password must be 6 characters or longer")
               .required("Password is required")
@@ -66,7 +94,7 @@ function LoginForm({errors, touched, Values}) {
         // handle submit
         handleSubmit(values) {
               axios
-                .post("https://reqres.in/api/users/", values)
+                .post("/api/auth/login", values)
                 .then(res => {
                   console.log(values); // Data was created successfully and logs to console
                 })
