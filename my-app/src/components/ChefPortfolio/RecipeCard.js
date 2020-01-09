8d4f92ebd43a30b01e010c00e41c3b3e2eee61e9
Jeff Breig <jjbreig@gmail.com>
2020-01-08 20:10:54 -0500

    finished styling recipe cards for now

--- my-app/src/components/ChefPortfolio/RecipeCard.js
+++ my-app/src/components/ChefPortfolio/RecipeCard.js
@@ -6,25 +6,54 @@ import styled from 'styled-components';
 const Card = styled.div`
     display: flex;
     flex-direction: column;
-    height: 300px;
+    height: 325px;
     width: 30%;
-    border: 1px solid red;
+    border: 1px solid lightgray;
     margin: 5px 5px;
 `;
 
 const CardTitle=styled.h2`
-    color: red;
+    color: #1a1a1a;
     font-size: 18px;
+    text-align: left;
+    border-bottom: 1px solid lightgray;
+    padding: 5px;
+`
+const CardAuthor=styled.h3`
+    color: #545454;
+    font-size: 14px;
+`
+const CardType=styled.div`
+    color: white;
+    background-color: #32B672;
+    border-radius: 20px;
+    font-size: 12px;
+    width: 33%;
     text-align: center;
-    margin: 8px auto 0 auto;
-    border: 1px solid blue;
+`
+const CardAuthorAndType=styled.div`
+    display: flex;
+    justify-content:space-between;
+    align-items: baseline;
+    padding: 5px;
+
 `
 
 const CardImage = styled.img`
     max-width: 250px;
     max-height:150px;
-    
+    min-height: 150px;
     overflow: hidden;
+`
+
+const CardDescription = styled.p`
+    font-size: 12px;
+    color: #1a1a1a;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    height: 15%;
+
 `
 
 const RecipeCard = ({recipe}) => {
@@ -33,6 +62,11 @@ const RecipeCard = ({recipe}) => {
         <Card>
             <CardImage src={recipe.image} />
             <CardTitle>{recipe.title}</CardTitle>
+            <CardAuthorAndType>
+                <CardAuthor>{recipe.author}</CardAuthor>
+                <CardType>{recipe.type}</CardType>
+            </CardAuthorAndType>
+            <CardDescription>{recipe.description}</CardDescription>
         
         </Card>
     )
